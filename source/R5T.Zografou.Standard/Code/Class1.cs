43815a723b8185b4d4fb8c1f9b819ddcb77360b1
MinexAutomation <minex.automation@gmail.com>
2020-01-22 00:22:14 -0800

    Added IServiceCollection extensions.

--- source/R5T.Zografou.Standard/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Zografou.Standard
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Zografou.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Zografou.Default;
+
+
+namespace R5T.Zografou.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddTestingDataDiretoryNameConvention(this IServiceCollection services)
+        {
+            services.AddSingleton<ITestingDataDirectoryNameConvention, TestingDataDirectoryNameConvention>();
+
+            return services;
+        }
+    }
+}
