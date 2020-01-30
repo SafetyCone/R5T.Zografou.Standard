a5e97f9417af3af7cc2d8a11cb36a01a1c51bf8e
MinexAutomation <minex.automation@gmail.com>
2020-01-30 14:37:45 -0800

    Updated IServiceCollection extensions to use ServiceAction<T> functionality.

--- source/R5T.Zografou.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
+++ source/R5T.Zografou.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -2,6 +2,7 @@
 
 using Microsoft.Extensions.DependencyInjection;
 
+using R5T.Dacia;
 using R5T.Zografou.Default;
 
 
@@ -9,11 +10,23 @@ namespace R5T.Zografou.Standard
 {
     public static class IServiceCollectionExtensions
     {
+        /// <summary>
+        /// Adds the <see cref="ITestingDataDirectoryNameConvention"/> service.
+        /// </summary>
         public static IServiceCollection AddTestingDataDiretoryNameConvention(this IServiceCollection services)
         {
-            services.AddSingleton<ITestingDataDirectoryNameConvention, TestingDataDirectoryNameConvention>();
+            services.AddDefaultTestingDataDirectoryNameConvention();
 
             return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="ITestingDataDirectoryNameConvention"/> service.
+        /// </summary>
+        public static ServiceAction<ITestingDataDirectoryNameConvention> AddTestingDataDiretoryNameConventionAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<ITestingDataDirectoryNameConvention>(() => services.AddDefaultTestingDataDirectoryNameConvention());
+            return serviceAction;
         }
     }
 }
